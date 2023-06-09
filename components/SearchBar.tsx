bb38cafb53c542bf07b74f719e13ac7e30958995
maurice <maurice.oluoch@naiparq.co.ke>
2023-06-09 16:30:19 +0300

    init

---
+++ components/SearchBar.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+const SearchBar = () => {
+  const handleSearch = () => {};
+  return <form action="" className="searchBar" onSubmit={handleSearch}></form>;
+};
+
+export default SearchBar;
